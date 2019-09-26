2eaf5e7ce8342e3751e08e4d5012b300e95e543c
adishthapa <adishthapa@outlook.com>
2019-09-26 17:34:17 -0400

    Started adding basic web components to the app

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center bg-light">
+      <h1 className="display-4">(React) Google Books Search</h1>
+      <p className="lead">Search for and Save Books of Interest!</p>
+    </div>
+  );
+}
+
+export default Jumbotron;
---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-md bg-light mb-3">
+      <span className="navbar-brand mb-0 h1">
+        <span className="blue">G</span>
+        <span className="red">o</span>
+        <span className="yellow">o</span>
+        <span className="blue">g</span>
+        <span className="green">l</span>
+        <span className="red">e</span>
+        &nbsp;
+        <u>Books</u>
+      </span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link pr-5" href="#">
+              Search
+            </a>
+          </li>
+          <li className="nav-item pl-5 pr-5">
+            <a className="nav-link" href="#">
+              Saved
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
